b535652eb2b1eb5e4f47a48ce2e4236b18163d27
GuildOfCalamity <81915293+GuildOfCalamity@users.noreply.github.com>
2024-12-04 19:16:15 -0500

    initial commit

---
+++ Source/Converters/NumberToString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MessageBarDemo.Converters;
+
+public class NumberToString : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (double.TryParse(value?.ToString(), out double val))
+        {
+            if (parameter != null)
+                return $"{parameter} speed is set to {(val == 0 ? "indefinite" : $"{val:N0} ms")}";
+            else
+                return $"Speed is set to {(val == 0 ? "indefinite" : $"{val:N0} ms")}";
+        }
+        return $"{value}";
+    }
+
+    public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var strValue = value as string;
+        if (strValue != null)
+        {
+            double result;
+            var converted = double.TryParse(strValue, out result);
+            if (converted)
+                return result;
+        }
+        return null;
+    }
+}
